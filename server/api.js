21687b68cb6d48bf77af4afd784910d0c0be76ed
yeb9925 <seungt.han@gmail.com>
2017-11-11 19:24:28 -0500

    api routes

--- server/api.js
+++ server/api.js
@@ -1,35 +1,81 @@
 const router = require('expres').Router();
-const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
-module.exporst = router;
+const key = require('../config');
+
+module.exports = router;
 
 //get all notes
 router.get('/', (req, res, next) => {
-    
+  const allNotes;
+  MongoClient.connect(key, (err, db) => {
+    allNotes = db.notes.find({});
+    db.close();
+  });
+  res.json(allNotes);
 });
 
 //get all notes for the specific day
 router.get('/:day', (req, res, next) => {
-
+  const dayNotes,
+        day = req.params.day;
+  MongoClient.connect(key, (err, db) => {
+    dayNotes = db.notes.find({ day });
+    db.close();
+  });
+  res.json(dayNotes);
 });
 
 //get just one note
 router.get('/:id', (req, res, next) => {
-    
+  const oneNote,
+        _id = req.params.id;
+  MongoClient.connect(key, (err, db) => {
+    oneNote = db.notes.findOne({ _id });
+    db.close();
+  });
+  res.json(oneNote);
 });
 
 //make a new note
 router.post('/', (req, res, next) => {
+  const topic = req.body.topic,
+        date = req.body.date,
+        content = req.body.content;
 
+  MongoClient.connect(key, (err, db) => {
+    db.notes.insert({ topic, date, content });
+    db.close();
+  });
 });
 
-//update the existing note
+//update an existing note
 router.put('/:id', (req, rex, next) => {
-
+  const _id = req.params.id,
+        updates = req.body;
+  MongoClient.connect(key, (err, db) => {
+    db.notes.findOneAndUpdate(
+      { _id }, //find by _id
+      updates // update the note
+    );
+    db.close();
+  });
 });
 
 //delete the existing note
 router.delete('/:id', (req, res, next) => {
+  const _id = req.params.id;
+  MongoClient.connect(key, (err, db) => {
+    db.notes.findOneAndDelete( { _id } );
+    db.close();
+  });
+});
 
+
+//error handling
+router.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 });
 
--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ if (process.env.NODE_ENV !== 'production') require('../secret');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public/index.html'));
-})
+});
+
 const createApp = () => {
   // body parsing middleware
   app.use(bodyParser.json());
@@ -20,13 +21,13 @@ const createApp = () => {
 // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {
-        const err = new Error('Not found');
-        err.status = 404;
-        next(err);
+      const err = new Error('Not found');
+      err.status = 404;
+      next(err);
     } else {
-        next();
+      next();
     }
-  })
+  });
 
 // error handling endware
   app.use((err, req, res, next) => {
@@ -34,19 +35,19 @@ const createApp = () => {
     console.error(err.stack);
     res.status(err.status || 500).send(err.message || 'Internal server error.');
   })
-}
+};
 
 const startListening = () => {
-    const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
-}
+  const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+};
   
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
 // It will evaluate false when this module is required by another module - for example,
 // if we wanted to require our app in a test spec
 if (require.main === module) {
-    createApp;
-    startListening();
+  createApp;
+  startListening();
 } else {
-createApp()
-}
+  createApp();
+};
