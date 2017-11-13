786d1ef9691a65623437813846719e23b73ec6f8
yeb9925 <seungt.han@gmail.com>
2017-11-13 11:18:45 -0500

    attemp to fix

--- server/index.js
+++ server/index.js
@@ -11,39 +11,34 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
-const createApp = () => {
-  // body parsing middleware
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
 
-  app.use('/api', require('./api'));
+
+// body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', require('./api'));
 
 // any remaining requests with an extension (.js, .css, etc.) send 404
-  app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found');
-      err.status = 404;
-      next(err);
-    } else {
-      next();
-    }
-  });
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
 
 // error handling endware
-  app.use((err, req, res, next) => {
-    console.error(err);
-    console.error(err.stack);
-    res.status(err.status || 500).send(err.message || 'Internal server error.');
-  })
-};
-
-const startListening = () => {
-  const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
-};
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+})
   
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
 // It will evaluate false when this module is required by another module - for example,
 // if we wanted to require our app in a test spec
-startListening();
-createApp();
