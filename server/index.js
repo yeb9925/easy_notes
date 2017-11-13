0180e7e92c8012552ff2768c57ba73556215a99a
yeb9925 <seungt.han@gmail.com>
2017-11-13 10:57:52 -0500

    changes

--- server/index.js
+++ server/index.js
@@ -11,40 +11,43 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
-// body parsing middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const createApp = () => {
+  // body parsing middleware
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api', require('./api'));
+  app.use('/api', require('./api'));
 
 // any remaining requests with an extension (.js, .css, etc.) send 404
-app.use((req, res, next) => {
-  if (path.extname(req.path).length) {
-    const err = new Error('Not found');
-    err.status = 404;
-    next(err);
-  } else {
-    next();
-  }
-});
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found');
+      err.status = 404;
+      next(err);
+    } else {
+      next();
+    }
+  });
 
 // error handling endware
-app.use((err, req, res, next) => {
-  console.error(err);
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error.');
-})
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+  })
+};
 
-
-// const startListening = () => {
-//   const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
-// };
+const startListening = () => {
+  const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+};
   
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
 // It will evaluate false when this module is required by another module - for example,
 // if we wanted to require our app in a test spec
-
-// startListening();
-
-app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+if (require.main === module) {
+  createApp();
+  startListening();
+} else {
+  createApp();
+};
